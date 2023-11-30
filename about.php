5cfe9c0266cc8b22d939634ccc1849952c1301b4
salizarana <salizarana@gmail.com>
2023-11-30 18:49:38 +0545

    Add about page

---
+++ about.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include ("partials/head.php");
+?>
+<body class="animsition">
+<?php
+	include ("partials/header.php");
+?>
+	<!-- Title page -->
+	<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('images/about01.jpg');">
+		<h2 class="ltext-105 cl0 txt-center" style="color:black">
+			About
+		</h2>
+	</section>	
+
+
+	<!-- Content page -->
+	<section class="bg0 p-t-75 p-b-120">
+		<div class="container">
+			<div class="row p-b-148">
+				<div class="col-md-7 col-lg-8">
+					<div class="p-t-7 p-r-85 p-r-15-lg p-r-0-md">
+						<h3 class="mtext-111 cl2 p-b-16">
+							Our Story
+						</h3>
+
+						<p class="stext-113 cl6 p-b-26">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat consequat enim, non auctor massa ultrices non. Morbi sed odio massa. Quisque at vehicula tellus, sed tincidunt augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas varius egestas diam, eu sodales metus scelerisque congue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas gravida justo eu arcu egestas convallis. Nullam eu erat bibendum, tempus ipsum eget, dictum enim. Donec non neque ut enim dapibus tincidunt vitae nec augue. Suspendisse potenti. Proin ut est diam. Donec condimentum euismod tortor, eget facilisis diam faucibus et. Morbi a tempor elit.
+						</p>
+
+						<p class="stext-113 cl6 p-b-26">
+							Any questions? Let us know in store at 8th floor, 379 Hudson, Lalitpur, Nepal or call us on (+977) 9812345678
+						</p>
+					</div>
+				</div>
+
+				<div class="col-11 col-md-5 col-lg-4 m-lr-auto">
+					<div class="how-bor1 ">
+						<div class="hov-img0">
+							<img src="images/about1.jpg" alt="IMG">
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</section>	
+<?php
+include ("partials/footer.php");
+?>
+	
+</body>
+</html>
