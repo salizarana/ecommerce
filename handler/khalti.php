08c12f9e96e2f19512c383d9f017267208a6d848
salizarana <salizarana@gmail.com>
2023-12-06 20:29:08 +0545

    Payment integration

---
+++ handler/khalti.php
@@ -0,0 +1,5 @@
+
+<script src="https://khalti.s3.ap-south-1.amazonaws.com/KPG/dist/2020.12.17.0.0.0/khalti-checkout.iffe.js"></script>
+<button onclick="window.location.href='khalti_v2.php';" id="payment-button" style="background-color:#5E338D; color:white">Pay with Khalti</button>
+
+
